2c9dee252b311c32f64ef75a90208dff23664c9c
Irfan81Bot <125128497+Irfan81Bot@users.noreply.github.com>
2023-02-17 18:50:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://dsbotzz:786or786@cluster0.wrbdm8z.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
-    SESSION = os.environ.get("SESSION", "BQAWtSrBW0y_PRG02vTt0DsKDzdg4moU3JqgwyUIX9-w_bpRYmvFouW9qyKA8UTCNi4h-Nq6kcwz-paPutR-sSzwwG4kZZeunWmmNVTVYbNrCnJGeahPGHUo8C_XU8RR52S78x1IYC_yHkm5h7fhk_U3Ign6z57XStMbCt2gvJ1KJcvhgZBX_wvOKyiYxmEK6jo7WiZ6PXe6Kvy9iFVCyxsf3rDEjkeZ0HugM2PHPoVvm6R3CfnOUjHNOICSXLzRgahi-IuE4WrPBEmjMRqhx1kpMmoTV6TPGW9kqB9ltpAirR6TRk_lmrGTn8j_rsrbwTSPo5hzklAk5auY6hR3gOkGAAAAAT8AbysA")
+    SESSION = os.environ.get("SESSION", "")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001751695482"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_Forword_DS_Bot")
 
